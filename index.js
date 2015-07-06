feb6e3ab206d7058b792e3646e560b8036820973
Marcus Jaschen <mjaschen@gmail.com>
2015-07-06 22:07:20 +0200

    Removed unused code

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 var self        = require('sdk/self');
 var pageMod     = require("sdk/page-mod");
-var data        = self.data;
-
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-function dummy(text, callback) {
-  callback(text);
-}
-
-exports.dummy = dummy;
 
 pageMod.PageMod({
     include: "*.strava.com",
