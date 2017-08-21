2496f97f8406d720dcd4a3862503914d32aea3ca
Marcus Jaschen <mjaschen@gmail.com>
2017-08-21 13:33:16 +0200

    fix: sticky global navbar

--- assets/js/strava_helper.navbar.js
+++ assets/js/strava_helper.navbar.js
@@ -3,8 +3,8 @@ var strava_helper = (function(strava_helper) {
     strava_helper.navbar = {
 
         holdNavbar: function() {
-            $(".container > header").css({"position": "fixed", "top": "0", "height": "55px"});
-            $(".container > .page").css({"top": "55px"});
+            $("#global-header").css({"position": "fixed", "width": "100%"});
+            $(".page.container").css({"margin-top": "55px"});
         },
 
         addHeatMapLink: function() {
