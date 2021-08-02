9b0afe1a47478ccf1163e82ce52d5de869abf42e
Marcus Jaschen <mjaschen@gmail.com>
2021-08-02 07:45:17 +0200

    fix give kudos to all (new markup)

--- assets/js/strava_helper.kudos_all.js
+++ assets/js/strava_helper.kudos_all.js
@@ -6,11 +6,33 @@ strava_helper = (function (strava_helper) {
         },
 
         giveKudosToAll: function () {
-            var count = $('button.js-add-kudo').length
-            $('button.js-add-kudo').trigger('click')
-            strava_helper.kudos_all.showKudosCount(count, true)
+            let kudos_count = 0
 
-            var timer = window.setTimeout(function () {
+            // search for all "Kudos" buttons on the current page ...
+            $('button[data-testid="kudos_button"]').each(function(index, element) {
+                // ... skip if Kudos where already given to an activity
+                if ($('svg[data-testid="unfilled_kudos"]', $(element)).length === 0) {
+                    return
+                }
+
+                // ... and skip own activities
+                let $container = $(element).parents('div.react-card-container').first()
+                // own activity which is hidden ("Only you can view this activity. It won't appear on segment leaderboards and may not count toward some challenges")
+                if ($('div[data-testid="visibility_section"]', $container).length > 0) {
+                    return
+                }
+                // own activity which is public (share dropdown exists)
+                if ($('div[data-testid="entry_actions_container"]', $container).length > 0) {
+                    return
+                }
+
+                $(element).trigger('click')
+                kudos_count++
+            })
+
+            strava_helper.kudos_all.showKudosCount(kudos_count, true)
+
+            window.setTimeout(function () {
                 strava_helper.kudos_all.resetKudosButton()
             }, 5000)
         },
